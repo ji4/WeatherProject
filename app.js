25d6b1fabfef04d98266aadb472a046471fbf687
ji4 <ji4@users.noreply.github.com>
2021-01-03 19:29:25 +0800

    Fetch Weather Data And Present It

--- app.js
+++ app.js
@@ -4,16 +4,22 @@ const https = require("https");
 const app = express();
 
 app.get("/", function(req, res){
-	const url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=271a7678dfb0e78920be91de92904957&units=metric";
+	const url = "https://api.openweathermap.org/data/2.5/weather?q=Taipei&appid=271a7678dfb0e78920be91de92904957&units=metric";
 	https.get(url, function(response){
 //		console.log(response);
 		
 		response.on("data", function(data){
-			const weatherData = JSON.parse(data);
-			console.log(weatherData);
+			const weatherData = JSON.parse(data)
+			const temp = weatherData.main.temp
+			const weatherDescription = weatherData.weather[0].description
+			const icon = weatherData.weather[0].icon
+			const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
+			res.write("<p>The weather is currently " + weatherDescription + ".</p>");
+			res.write("<h1>The temperature in Taipei is " + temp + " degrees Celcius.</h1>");
+			res.write("<img src= " + imageURL + ">");
+			res.send()
 		})
 	})
-	res.send("Server is up and running.");
 });
 
 app.listen(3000, function(){
