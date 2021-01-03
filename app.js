b082fe10121266069727f92098c1d0e7996e4ab2
ji4 <ji4@users.noreply.github.com>
2021-01-03 20:01:54 +0800

    Parse Post Query

--- app.js
+++ app.js
@@ -8,32 +8,29 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
 	res.sendFile(__dirname + "/index.html");
-
 });
 
 app.post("/", function(req, res){
-	console.log("Post Request Received.");
+	const query = req.body.cityName;
+	const apiKey = "271a7678dfb0e78920be91de92904957";
+	const unit = "metric";
+	const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + unit;
+	https.get(url, function(response){
+		response.on("data", function(data){
+			const weatherData = JSON.parse(data);
+			const temp = weatherData.main.temp;
+			const weatherDescription = weatherData.weather[0].description;
+			const icon = weatherData.weather[0].icon;
+			const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+			res.write("<p>The weather is currently " + weatherDescription + ".</p>");
+			res.write("<h1>The temperature in " + query + " is " + temp + " degrees Celcius.</h1>");
+			res.write("<img src= " + imageURL + ">");
+			res.send()
+		})
+	})
 });
 
-//	const query = "Taipei";
-//	const apiKey = "271a7678dfb0e78920be91de92904957";
-//	const unit = "metric";
-//	const url = "https://api.openweathermap.org/data/2.5/weather?q=" + Taipei + "&appid=" + apiKey + "&units=" + unit;
-//	https.get(url, function(response){
-////		console.log(response);
-//		
-//		response.on("data", function(data){
-//			const weatherData = JSON.parse(data)
-//			const temp = weatherData.main.temp
-//			const weatherDescription = weatherData.weather[0].description
-//			const icon = weatherData.weather[0].icon
-//			const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
-//			res.write("<p>The weather is currently " + weatherDescription + ".</p>");
-//			res.write("<h1>The temperature in Taipei is " + temp + " degrees Celcius.</h1>");
-//			res.write("<img src= " + imageURL + ">");
-//			res.send()
-//		})
-//	})
+
 
 app.listen(3000, function(){
 	console.log("Server is running on port 3000.")
