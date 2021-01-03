095c749485923c2be6cef63e0c6f20bb8fbd7a04
ji4 <ji4@users.noreply.github.com>
2021-01-03 20:00:02 +0800

    Require body-parser

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
 	res.sendFile(__dirname + "/index.html");
